0943cd219f9c803bad8367ae1230b47ce6ace290
Muhammad Bashir Ibrahim <mbashiribrahim7@gmail.com>
2025-05-03 10:31:31 +0100

    fix linting errors

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require("express");
-const app = express();
+/* eslint-disable no-undef */
+const express = require('express')
+
+const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+/* eslint-disable no-undef */
+const PORT = process.env.PORT || 5000
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+  /* eslint-disable no-console*/
+  console.log(`server started on port ${PORT}`)
+})
